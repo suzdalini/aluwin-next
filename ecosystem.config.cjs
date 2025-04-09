3375ee4f37b5eeef136aa78206b0a7ae0b063ca4
Suzdalini <suzdalini@gmail.com>
2025-04-09 18:15:50 +0300

    first commit

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,8 +1,9 @@
+// ecosystem.config.cjs
 module.exports = {
     apps: [
         {
             name: 'alu-win',
-            script: 'node_modules/next/dist/bin/next',
+            script: './node_modules/next/dist/bin/next',
             args: 'start',
             cwd: '/var/www/alu-win/src-react',
             env: {
