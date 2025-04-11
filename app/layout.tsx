7ab7b5ad697d1d43841ad6c9e7b5f8b8293fffdf
Suzdalini <suzdalini@gmail.com>
2025-04-11 08:24:27 +0300

    callibri script

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,31 +9,32 @@ import { YandexMetrika } from '@/components/metrika/YandexMetrika';
 import Image from 'next/image';
 import UtmTracker from '@/components/metrika/UtmTracker';
 const montserrat = Montserrat({
-  subsets: ['cyrillic'],
-  variable: '--font-montserrat',
-  weight: ['300', '400', '500', '600', '700'],
+    subsets: ['cyrillic'],
+    variable: '--font-montserrat',
+    weight: ['300', '400', '500', '600', '700'],
 });
 
 export default async function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-	//const cookieStore = await cookies();
-	//const variant = cookieStore.get('ab_variant')?.value || 'A';
-	//const variant = 'A';
+    //const cookieStore = await cookies();
+    //const variant = cookieStore.get('ab_variant')?.value || 'A';
+    //const variant = 'A';
 
-	return (
-		<html lang="ru">
-		<head>
-			<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-			<link rel="icon" href="/favicon.svg" type="image/svg+xml" />
-		</head>
-		<body className={`${montserrat.variable} antialiased`}>
-			<UtmTracker />
-			{/* Яндекс.Метрика */}
-			<Script id="yandex-metrika" strategy="afterInteractive">
-			{`
+    return (
+        <html lang="ru">
+            <head>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+                <Script src="//cdn.callibri.ru/callibri.js" strategy="afterInteractive" defer />
+            </head>
+            <body className={`${montserrat.variable} antialiased`}>
+                <UtmTracker />
+                {/* Яндекс.Метрика */}
+                <Script id="yandex-metrika" strategy="afterInteractive">
+                    {`
 				(function(m,e,t,r,i,k,a){
 				m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
 				m[i].l=1*new Date();
@@ -49,38 +50,38 @@ export default async function RootLayout({
 				webvisor:true
 				});
 			`}
-			</Script>
+                </Script>
 
-			<noscript>
-			<div>
-				<Image
-				src={`https://mc.yandex.ru/watch/${process.env.NEXT_PUBLIC_METRIKA_ID}`}
-				alt=""
-				width={1}
-				height={1}
-				style={{ position: 'absolute', left: '-9999px' }}
-				/>
-			</div>
-			</noscript>
+                <noscript>
+                    <div>
+                        <Image
+                            src={`https://mc.yandex.ru/watch/${process.env.NEXT_PUBLIC_METRIKA_ID}`}
+                            alt=""
+                            width={1}
+                            height={1}
+                            style={{ position: 'absolute', left: '-9999px' }}
+                        />
+                    </div>
+                </noscript>
 
-			{/* A/B-провайдер с вариантом из cookie */}
-			{/* <ABTestProvider variant={variant}> */}
-			<YandexMetrika />
+                {/* A/B-провайдер с вариантом из cookie */}
+                {/* <ABTestProvider variant={variant}> */}
+                <YandexMetrika />
 
-			<div className="sticky top-0 z-50 bg-white">
-				<div className="container mx-auto px-5 md:px-0">
-				<Header />
-				</div>
-			</div>
+                <div className="sticky top-0 z-50 bg-white">
+                    <div className="container mx-auto px-5 md:px-0">
+                        <Header />
+                    </div>
+                </div>
 
-			<div className="h-6"></div>
+                <div className="h-6"></div>
 
-			<div className="container mx-auto px-5 md:px-0">
-				{children}
-				<Footer />
-			</div>
-			{/* </ABTestProvider> */}
-		</body>
-		</html>
-	);
+                <div className="container mx-auto px-5 md:px-0">
+                    {children}
+                    <Footer />
+                </div>
+                {/* </ABTestProvider> */}
+            </body>
+        </html>
+    );
 }
